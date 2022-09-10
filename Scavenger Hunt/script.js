fb30d65b485384a065b31c7cc3b2be87b52a8907
Sanjay71013 <sanjaymythili2002@gmail.com>
2022-09-10 22:02:40 +0530

    Initialsing

---
+++ Scavenger Hunt/script.js
@@ -0,0 +1,5 @@
+function encrypt() {
+    var p = document.getElementById("txt");
+    p.innerHTML = "Couldn't find any Hackers, Try Harder!!, Don't lose your hope Aiden Pearce...";
+}
+/* Third part of the flag is afn_1j09} */
